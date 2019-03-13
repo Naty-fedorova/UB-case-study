8cdbe820dff087a75966d1a9ffa7c2595cd9cafb
Natalia Fedorova <nataliafedora23@gmail.com>
2019-03-13 18:43:51 +0100

    working model with land buying and house building

--- ub_sim.R
+++ ub_sim.R
@@ -105,17 +105,24 @@ sim_ub <- function( tmax=10 , N_plots=100 , N_migrants=20 , plot_capacity=2 , pl
   intend_stay <- rnorm(N_hh)
   residence_length_plot <- rep(0, times = N_hh)
   residence_length_total <- rep(0, times = N_hh)
+  in_env <- rep(0, time = N_hh)
+  house_invest <- rep(0, time = N_hh)
   
-  hh_df <- data.frame(hh_id, fam_id, capital, intend_stay, residence_length_plot, residence_length_total)
+  hh_df <- data.frame(hh_id, fam_id, capital, intend_stay, residence_length_plot, residence_length_total, in_env, house_invest)
   
   # init plots
   plot_pop <- rep( 0 , N_plots ) #TODO remove this, can just use length of plot_ids row
+  plot_own <- rep( 0 , N_plots )
   plot_house <- rep( 0 , N_plots )
   plot_ids <- matrix( 0 , nrow=N_plots , ncol=plot_capacity ) #matrix of plots to be filled with hh ids
   
   # loop
   for ( t in 1:tmax ) {
     print(c("Iteration ", t))
+    
+    #TODO 
+    # stochastic change to capital at start of each time step
+    # remove squatters that did not buy land
     
     # finding land
     for ( i in 1:N_migrants ) {
@@ -151,6 +158,9 @@ sim_ub <- function( tmax=10 , N_plots=100 , N_migrants=20 , plot_capacity=2 , pl
           print(hh_df)
         }
         plot_ids[destination , plot_pop[destination]] <- hh_df$hh_id[hh_index]
+        
+        #update hh_df to show which agents are now in the environment/occupying plots
+        hh_df$in_env[hh_index] <- 1
       }
       else {
         print(c("The possible plots are full for id: ", hh_df$hh_id[hh_index]))
@@ -158,17 +168,59 @@ sim_ub <- function( tmax=10 , N_plots=100 , N_migrants=20 , plot_capacity=2 , pl
     }# finding land
     
     # buying land
+    # if live on available land from previous round, and have sufficient capital - buy land
+    # if live on family land already for *some time*, look for new land
+    
+    #go through plots
+    for(i in 1:nrow(plot_ids)){
+      # get own occupied patches
+      if(plot_ids[i,1] > 0){
+        # if their capital is more than 0, they can buy the land (stochastic)
+        if(hh_df$capital[plot_ids[i,1]] > 0 & plot_own[i] == 0){
+          plot_own[i] <- rbinom(1, 1, 0.7) #stochastic
+        }
+      }
+      
+    }
+    
+    # building home
+    # if live on family land stay in ger, so ignore
+    # if live on own land, can build
+    # investment proportional to capital and intended stay
+    # min capital needed for building house:0 
+    for(i in 1:length(plot_own)){
+      if(plot_own[i] == 1){
+        
+        if(plot_house[i] == 0){
+          plot_house[i] <- rbinom(1, 1, 0.7)
+          
+          if(plot_house[i] == 1){
+            hh_df$house_invest[plot_ids[i,1]] <- rnorm(1, hh_df$capital[plot_ids[i,1]] + hh_df$intend_stay[plot_ids[i,1]] )
+          }
+        }
+      }
+    }
+    
+
+    
+
+    
+    
+    
+    #TODO end of each timestep - update total residence
+    #TODO can only squat for one timestep
     
   }#t
   
   return(
     list(
-      plot_pop = plot_pop,
-      plot_house = plot_house,
-      ids = plot_ids
+      plot_own = plot_own,
+      plot_house = plot_house, 
+      ids = plot_ids,
+      hh_df = hh_df
     )
   )
   
 }
 
-s <- sim_ub(tmax=20,N_plots=100)
+s <- sim_ub(tmax=10,N_plots=100)
